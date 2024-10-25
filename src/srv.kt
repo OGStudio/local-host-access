3ddb132777a08a5b3693c2f36aa7ccfa6a44f1f5
Михаил Капелько <kornerr@gmail.com>
2024-10-25 23:09:06 +0300

    d

--- src/srv.kt
+++ src/srv.kt
@@ -6,6 +6,13 @@ import io.ktor.server.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.cio.*
 
+var globalCall: ApplicationCall? = null
+
+suspend fun doReply() {
+    val path = globalCall?.parameters["path"]
+    globalCall?.respondText("Hello, World!. Path: '$path'")
+}
+
 /**
  * Run simple HTTP server
  */
@@ -15,8 +22,11 @@ fun srvRunHTTPServer(
     embeddedServer(CIO, port) {
         routing {
             get("/{path?}") {
-                val path = call.parameters["path"]
-                call.respondText("Hello, World!. Path: '$path'")
+                globalCall = call
+                
+                doReply()
+                //val path = this.call.parameters["path"]
+                //call.respondText("Hello, World!. Path: '$path'")
             }
         }
     }.start(wait = true)
