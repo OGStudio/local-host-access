05f31acdd75dd2064a4e8c316d8a7085aab985b3
Михаил Капелько <kornerr@gmail.com>
2024-11-03 22:11:51 +0300

    d

--- src/srv.kt
+++ src/srv.kt
@@ -1,10 +1,13 @@
 package org.opengamestudio
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import io.ktor.server.engine.*
 import io.ktor.server.cio.*
+import io.ktor.server.request.httpMethod
+import io.ktor.server.request.receive
 import io.ktor.server.request.uri
 
 /**
@@ -15,9 +18,25 @@ fun srvRunHTTPServer(p: Platform) {
     val srv: ApplicationEngine = embeddedServer(CIO, p.c.httpPort) {
         routing {
             get("/{path...}") {
+                val url = call.request.uri
+                val method = call.request.httpMethod
+                val body = ""
+
+                /**/println("ИГР GET url/method/body: '$url'/'$method'/'$body'")
+
                 // The call to `httpPath` results in synchronous
                 // execution of logic that updates `p.c.httpReply`
                 // indirectly.
+                p.ctrl.set("httpPath", call.request.uri)
+                call.respondText(p.c.httpReply)
+            }
+
+            post("/{path...}") {
+                val url = call.request.uri
+                val method = call.request.httpMethod
+                val body = call.receive<String>()
+                /**/println("ИГР POST url/method/body: '$url'/'$method'/'$body'")
+
                 p.ctrl.set("httpPath", call.request.uri)
                 call.respondText(p.c.httpReply)
             }
