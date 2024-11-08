0d807663fb5b49d269e40c3dd29b9dd22f8bdebc
Михаил Капелько <kornerr@gmail.com>
2024-11-08 23:16:35 +0300

    d

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,21 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "local-host-access"
 //include("app")
 include("nt")
