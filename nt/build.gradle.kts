4a6637da60b0673f903402b1225335926147a7a3
Михаил Капелько <kornerr@gmail.com>
2024-11-09 22:14:58 +0300

    d

--- nt/build.gradle.kts
+++ nt/build.gradle.kts
@@ -1,6 +1,9 @@
 val json_version: String by project
 val ktor_version: String by project
 
+val hostArch = System.getProperty("os.arch")
+val hostOS = System.getProperty("os.name")
+
 plugins {
     kotlin("multiplatform") version "2.0.20"
     // JSON parsing.
@@ -12,8 +15,14 @@ repositories {
 }
 
 kotlin {
-    //macosX64("native") {
-    mingwX64("native") {
+    val nativeTarget = when {
+        hostOS == "Mac OS X" && hostArch == "x86_64" -> macosX64("native")
+        hostOS == "Mac OS X" && hostArch == "aarch64" -> macosArm64("native")
+        hostOS == "Linux" -> linuxX64("native")
+        hostOS == "Windows" && hostArch == "x86_64" -> mingwX64("native")
+        else -> throw GradleException("Host OS+Arch is not yet supported")
+    }
+    nativeTarget {
         binaries {
             executable()
         }
