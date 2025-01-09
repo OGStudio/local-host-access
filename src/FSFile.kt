8aa50d0796059be28496ebe1122781540b609b73
Михаил Капелько <kornerr@gmail.com>
2025-01-09 22:36:41 +0300

    d

--- src/FSFile.kt
+++
@@ -1,7 +0,0 @@
-package org.opengamestudio
-
-data class FSFile(
-    var isDirectory: Boolean = false,
-    var isFile: Boolean = false,
-    var name: String = "",
-) {}
--- src/entities.kt
+++ src/entities.kt
@@ -1,5 +1,9 @@
 package org.opengamestudio
-
+data class FSFile(
+    var isDirectory: Boolean = false,
+    var isFile: Boolean = false,
+    var name: String = "",
+) {}
 data class NetRequest(
     var body: String = "",
     var method: String = "",
