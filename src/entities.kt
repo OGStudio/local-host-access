2878b20a74e7d3ec7066bc8859d41537acedaa32
Михаил Капелько <kornerr@gmail.com>
2025-02-23 22:01:38 +0300

    d

--- src/entities.kt
+++ src/entities.kt
@@ -1,9 +1,13 @@
 package org.opengamestudio
+
+// File system item representation
 data class FSFile(
     var isDirectory: Boolean = false,
     var isFile: Boolean = false,
     var name: String = "",
 ) {}
+
+// Client request representation
 data class NetRequest(
     var body: String = "",
     var method: String = "",
